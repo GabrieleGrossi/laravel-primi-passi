a6f786376550ef4072b39d861fa62802b52fe08c
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-18 15:32:58 +0200

    first commit

---
+++ resources/views/hello-world.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World Laravel</title>
+</head>
+<body>
+    <h1>
+        First page in Laravel 
+    </h1>
+</body>
+</html>
